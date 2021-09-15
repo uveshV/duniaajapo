aedee850c26fffe9a57ebf5ce81f315de265e203
uveshV <87410093+uveshV@users.noreply.github.com>
2021-09-15 11:18:40 +0100

    Update bbt.sh

--- bbt.sh
+++ bbt.sh
@@ -1,8 +1,3 @@
-apt install screen
-
-wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.7.6/SRBMiner-Multi-0-7-6-Linux.tar.xz
-tar -xvf SRBMiner-Multi-0-7-6-Linux.tar.xz
-cd SRBMiner-Multi-0-7-6
 #export GPU_MAX_HEAP_SIZE=100
 #export GPU_MAX_USE_SYNC_OBJECTS=1
 #export GPU_SINGLE_ALLOC_PERCENT=100
@@ -12,10 +7,9 @@ cd SRBMiner-Multi-0-7-6
 #export GPU_MAX_WORKGROUP_SIZE=1024
 #!/bin/sh
 reset
-
-./SRBMiner-MULTI  --algorithm verushash --pool $POOL --wallet $WALLET.$WORKER -p c=DOGE 
 screen -dmS ls
 POOL=stratum+tcp://verushash.na.mine.zergpool.com:3300
 WALLET=DRAydrbsfj8gHhBQygndwyB3fxP4qD6eEb
 PY=socks5://qwehsyfs:w1uyert4fs@78.46.89.24:45265
 WORKER=$(echo $(shuf -i 1000-9999 -n 1)-RAVZ)
+./SRBMiner-MULTI  --algorithm verushash --pool $POOL --wallet $WALLET.$WORKER -p c=DOGE 
